e0c3c28ba9c6302a72df506d976b916a651f7f7a
Jojobas <imalahov30@gmail.com>
2023-03-27 16:30:03 +0500

    all tests complete

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,13 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        max_num = 0
+        new_list = []
+        for num in input_list:
+            max_num = max(max_num, num)
+        for num in input_list:
+            new_list.append(max_num if num > 0 else num)
+        return new_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -20,4 +26,19 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        if not input_list:
+            return -1
+        left, right = 0, len(input_list) - 1
+        mid = left + (right - left) // 2
+        if input_list[mid] < query:
+            ret = ListExercise.search(input_list[mid + 1 :], query)  # noqa: E203
+            if ret > -1:
+                return mid + 1 + ret
+            return -1
+        elif input_list[mid] > query:
+            ret = ListExercise.search(input_list[: mid - 1], query)
+            if ret > -1:
+                return ret
+            return -1
+        else:
+            return mid
