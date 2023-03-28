400baac159bfeef3b5725d77ce62dda57c491ec6
Jojobas <imalahov30@gmail.com>
2023-03-28 11:32:56 +0500

    maps tests complete

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,22 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        def process_movie(movie: dict) -> float:
+            if movie["rating_kinopoisk"] and len(movie["country"].split(",")) > 1:
+                return float(movie["rating_kinopoisk"])
+            return 0.0
+
+        ratings = map(process_movie, list_of_movies)
+        num_movies = 0
+        total_rating = 0.0
+        for rating in ratings:
+            if rating >= 1:
+                num_movies += 1
+                total_rating += rating
+        if num_movies:
+            return total_rating / num_movies
+        return 0.0
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +43,15 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        def process_movie(movie: dict) -> int:
+            if (
+                movie["rating_kinopoisk"]
+                and float(movie["rating_kinopoisk"]) >= rating
+                and movie["name"]
+            ):
+                return movie["name"].count("и")
+            return 0
+
+        counts = map(process_movie, list_of_movies)
+        return sum(counts)
